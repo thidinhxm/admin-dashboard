0fab5722e1193e2c589303cdc5d1e3ee2a2000c8
thidinhxm <thidinhxm@gmail.com>
2022-04-15 17:47:24 +0700

    new page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { productInputs, userInputs } from './formSource';
 import Home from './pages/home/Home';
 import List from './pages/list/List';
 import Login from './pages/login/Login';
@@ -18,12 +19,12 @@ function App() {
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route path="new" element={<New inputs={userInputs} title="Add New User"/>} />
             </Route>
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route path="new" element={<New inputs={productInputs} title="Add New Product"/>} />
             </Route>
           </Route>
           
